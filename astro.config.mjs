107800bb6bcf5f04ee52440f8536a69b151abf11
alexroz555 <114452472+alexroz555@users.noreply.github.com>
2025-05-08 14:30:56 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://github.com/alexroz555/",
+  site: "https://alexroz555.github.io",
   base: "/demo-github-actions",
 });
